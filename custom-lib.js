aab4fed650d68bfa1e20463b9525f73e9c40c59d
acjm <chafaiayman@gmail.com>
2019-11-27 22:10:46 +0100

    separate files into modules

--- custom-lib.js
+++ custom-lib.js
@@ -1,30 +1,4 @@
-/**
- * 
- * @param {string} type of element to add
- * @param {object} props 
- * @param  {...object|string} children
- */
-function createElement(type, props, ...children) {
-    return {
-        type,
-        props: {
-            ...props, 
-            children: children.map(child => {
-                return typeof child === 'object' ? child : createTextElement(child)
-            })
-        }
-    }
-}
-
-function createTextElement(text) {
-    return {
-        type: 'TEXT_ELEMENT',
-        props: {
-            nodeValue: text, 
-            children: []
-        }
-    }
-}
+import { createElement } from './customlib/vdom.js'
 
 window.customLib = {
     createElement,
---
+++ customlib/vdom.js
@@ -0,0 +1,32 @@
+/**
+ * 
+ * @param {string} type of element to add
+ * @param {object} props 
+ * @param  {...object|string} children
+ */
+function createElement(type, props, ...children) {
+    return {
+        type,
+        props: {
+            ...props, 
+            children: children.map(child => {
+                return typeof child === 'object' ? child : createTextElement(child)
+            })
+        }
+    }
+}
+/**
+ * create a node type text
+ * @param {string} text 
+ */
+function createTextElement(text) {
+    return {
+        type: 'TEXT_ELEMENT',
+        props: {
+            nodeValue: text, 
+            children: []
+        }
+    }
+}
+
+export default createElement; 
